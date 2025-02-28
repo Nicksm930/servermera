249b94c1282384236dda426a60b3098657cb2e57
nikhilmore0712 <nikhil.more@neebal.com>
2025-02-28 14:49:49 +0530

    TASK:DB CONFID ADDED

---
+++ database/dbConfig.js
@@ -0,0 +1,14 @@
+const { Client } = require('pg');
+
+// Use the DATABASE_URL environment variable, which should be set according to your environment.
+const connectionString = process.env.DATABASE_URL;
+
+const client = new Client({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false, // Required for SSL on Render
+  },
+});
+
+
+  export default client;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const cors=require('cors')
+const client=require('./database/dbConfig.js')
 require('dotenv').config()
 
 const app=express()
@@ -14,6 +15,21 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(port,()=>{
+    client.connect()
+    .then(() => {
+      console.log("Connected to the database");
+      return client.query('SELECT NOW()');
+    })
+    .then((res) => {
+      console.log('Database time:', res.rows[0]);
+    })
+    .catch((err) => {
+      console.error('Error connecting to the database', err.stack);
+    })
+    .finally(() => {
+      client.end();
+    });
+  
     console.log("Mera Server Running");
 })
 
