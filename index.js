61d2499054c42a60844a063a8666581c65fb2a77
nikhilmore0712 <nikhil.more@neebal.com>
2025-02-28 12:15:16 +0530

    Task:update in remote dev

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("1.Commit after remote push")
+console.log("1.Commit in develop remote and compare with master remote")
 console.log("commiting in dev")
 console.log("Running mera server")
