311ba1e5607a3c17a835deb607193ae88a537484
nikhilmore0712 <nikhil.more@neebal.com>
2025-02-28 15:05:46 +0530

    Task:Resolved

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,7 +3,7 @@
 const { Client } = require('pg'); // Import pg Client
 
 // Use the DATABASE_URL environment variable, which should be set according to your environment.
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.INTERNAL_DATABASE_URL;
 
 const client = new Client({
   connectionString,
